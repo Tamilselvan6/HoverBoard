d68c072358521e23687556c74b524128a153cac2
Tamilselvan V <126091412+Tamilselvan6@users.noreply.github.com>
2025-01-06 09:10:18 +0530

    Added Touching Time delay

--- script.js
+++ script.js
@@ -12,40 +12,49 @@ const setColor = (square) => {
 };
 
 const removeColor = (square) => {
-    setTimeout(() => {
-      square.style.background = "#1d1d1d";
-      square.style.boxShadow = "0 0 2px #000";
-    }, 300); 
-  };
-  
+  setTimeout(() => {
+    square.style.background = "#1d1d1d";
+    square.style.boxShadow = "0 0 2px #000";
+  }, 300);
+};
 
 for (let i = 0; i < SQUARES; i++) {
-    
   const square = document.createElement("div");
   square.classList.add("square");
   square.addEventListener("mouseover", () => setColor(square));
   square.addEventListener("mouseout", () => removeColor(square));
   container.appendChild(square);
 }
-container.addEventListener("touchmove", (e) => {
-    const touch = e.touches[0];
-    const element = document.elementFromPoint(touch.clientX, touch.clientY);
-    if (element && element.classList.contains("square")) {
-        setColor(element);
-    }
-});
+
 hoverAllButton.addEventListener("click", () => {
-    const squares = document.querySelectorAll(".square");
-    squares.forEach((square, index) => {
-      setTimeout(() => {
-        setColor(square);
-      }, index * 10);
-    });
-  
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square, index) => {
     setTimeout(() => {
-      squares.forEach(square => {
-        removeColor(square);
-      });
-    }, 6000);
+      setColor(square);
+    }, index * 10);
   });
-  
+
+  setTimeout(() => {
+    squares.forEach(square => {
+      removeColor(square);
+    });
+  }, 6000);
+});
+
+let isTouching = false;
+container.addEventListener("touchmove", (e) => {
+  if (isTouching) return;
+  isTouching = true;
+
+  const touch = e.touches[0];
+  const element = document.elementFromPoint(touch.clientX, touch.clientY);
+  if (element && element.classList.contains("square")) {
+    setColor(element);
+    setTimeout(() => {
+      removeColor(element);
+      isTouching = false; 
+    }, 1000);
+  }
+
+  setTimeout(() => isTouching = false, 50);
+});
